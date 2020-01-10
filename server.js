77770be2506033117ac11a6f2ea2aeb96494e0a0
Zubair Naseer <zubairnaseer88@gmail.com>
2020-01-10 16:52:24 +0500

    Fixed

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const User = require("./User");
+
+const app = express();
+
+
+app.use(bodyParser.urlencoded({
+   extended: true
+}));
+
+mongoose.connect("mongodb+srv://zubair:zubair123@cluster0-clyyy.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(()=>{
+        console.log("MongoDB Connected");
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+
+// Add body parser
+app.use(bodyParser.json());
+app.get("/", (req, res) => {
+    res.send("hello to boiler plate");
+});
+
+app.post("/", (req, res) => {
+    // creating new user from User model
+    const newUser = new User({
+        firstName: req.body.firstName,
+        lastName:  req.body.lastName,
+        email: req.body.email,
+        phoneNumber: req.body.phoneNumber,
+        password: req.body.password
+    });
+
+    console.log(req.body);
+    console.log(newUser);
+
+    newUser.save()
+        .then((user) => {
+            res.json(user) // Sending back new user data
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+})
+
+
+app.listen(3030, (req,res)=>{
+    console.log(`running on port 3030`);
+})
